826d16e3497d2afb98d4494fc3d125e00f1f3061
gerisedlar15 <gerisedlar15@gmail.com>
2026-08-04 19:36:42 -0300

    Agregado registroPlanner definitivo, correccion de enlaces y unificacion del servidor

--- frontend/script.js
+++ frontend/script.js
@@ -4,19 +4,26 @@ const formulario = document.getElementById('formularioPlanner');
 formulario.addEventListener('submit', async function(evento) {
     evento.preventDefault(); // Frenamos la recarga de la página
 
-    const inputNombre = document.getElementById('nombrePlanner');
+    const inputNombre = document.getElementById('nombre');
     const nombreIngresado = inputNombre.value;
+    const inputUsuario = document.getElementById('usuario');
+    const usuarioIngresado = inputUsuario.value;
+    const inputContrasenia = document.getElementById('contrasenia');
+    const contraseniaIngresada = inputContrasenia.value;
 
     console.log("Enviando dato al backend:", nombreIngresado);
+    console.log("Enviando dato al backend:", usuarioIngresado);
+    console.log("Enviando dato al backend:", contraseniaIngresada);
+    
 
     try {
         // Usamos fetch para llamar a la puerta de tu API
-        const respuesta = await fetch('http://localhost:3001/api/planner', {
+        const respuesta = await fetch('http://localhost:3000/api/planner', {
             method: 'POST', // "Quiero crear algo nuevo"
             headers: {
                 'Content-Type': 'application/json' // Le avisamos que le mandamos un paquete de texto JSON
             },
-            body: JSON.stringify({ nombre: nombreIngresado }) // Armamos la cajita con el nombre
+            body: JSON.stringify({ nombre: nombreIngresado, usuario: usuarioIngresado, contrasenia: contraseniaIngresada }) // Armamos la cajita con los datos
         });
 
         // Verificamos si el backend nos dio el ok
@@ -25,6 +32,8 @@ formulario.addEventListener('submit', async function(evento) {
             console.log("respondio el back", resultado);
             alert("Planner guardado. ID asignado: " + resultado.id); 
             inputNombre.value = ""; // Limpiamos el casillero para el próximo
+            inputUsuario.value = ""; // Limpiamos el casillero para el próximo
+            inputContrasenia.value = ""; // Limpiamos el casillero para el próximo
         } else {
             alert(" Hubo un error al guardar");
         }
